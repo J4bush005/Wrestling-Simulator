73aaf2c9ec4e75f2b5ba20ac2f443bd88efa5617
J4bush005 <104151154+J4bush005@users.noreply.github.com>
2022-05-31 20:00:35 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+     public static void main(String[] args) {
+        Wrestler s = new Wrestler();
+        s.assignHeight();
+        s.assignId();
+        s.assignWeight();
+        s.assignStyle();
+    } 
+      
+    
+ }
